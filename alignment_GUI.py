7ca5de242c6d27ba7b6a0cd9a15e1cb2bd079bd2
Harsh Mathur <harshmathur.1990@gmail.com>
2025-06-23 17:37:02 +0000

    example alignment Ca with He

--- alignment_GUI.py
+++ alignment_GUI.py
@@ -600,10 +600,10 @@ if __name__ == '__main__':
 
     base_path = Path('/mnt/f/GRIS')
 
-    filename1 = '25Apr25ARM1-004.fits_squarred_pixels.fits_aligned_downsampled_streamed.fits'
+    filename1 = '25Apr25ARM1-003.fits_squarred_pixels.fits'
 
-    filename2 = '25Apr25ARM2-004.fits_squarred_pixels.fits_aligned_downsampled_streamed.fits'
+    filename2 = '25Apr25ARM2-003.fits_squarred_pixels.fits'
 
-    subpixel_accuracy = 1
+    subpixel_accuracy = 20
 
     animate(base_path, filename1, filename2, subpixel_accuracy)
